ef553f67a6464f3b62569f65a46c431403293b80
krze <kenkrzeminski@gmail.com>
2018-11-17 10:42:15 -0500

    Project created

---
+++ CP2020-Character-Sheet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CP2020-Character-Sheet
+//
+//  Created by Ken Krzeminski on 11/17/18.
+//  Copyright © 2018 Ken Krzeminski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
