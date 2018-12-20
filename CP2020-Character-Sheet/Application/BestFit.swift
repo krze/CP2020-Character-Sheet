44a851bb13c42f110d9b1468d5f2afb29854924b
krze <kenkrzeminski@gmail.com>
2018-12-19 20:01:13 -0500

    Maximum font size limit for the autoresize helper

--- CP2020-Character-Sheet/Application/BestFit.swift
+++ CP2020-Character-Sheet/Application/BestFit.swift
@@ -9,14 +9,21 @@ extension UIFont {
     ///   - bounds: Bounds of the view to fit
     ///   - fontDescriptor: The font's descriptor
     ///   - additionalAttributes: Additional NSAttributedString attributes
+    ///   - maximumFontSize: An optional limit to the upper bounds of your max font size.
     /// - Returns: GGFloat representing the font size that best fits in the bounds
-    static func bestFittingFontSize(for text: String, in bounds: CGRect, fontDescriptor: UIFontDescriptor, additionalAttributes: [NSAttributedString.Key: Any]? = nil) -> CGFloat {
+    static func bestFittingFontSize(for text: String, in bounds: CGRect, fontDescriptor: UIFontDescriptor, additionalAttributes: [NSAttributedString.Key: Any]? = nil, maximumFontSize: CGFloat? = nil) -> CGFloat {
         let constrainingDimension = min(bounds.width, bounds.height)
         let properBounds = CGRect(origin: .zero, size: bounds.size)
         var attributes = additionalAttributes ?? [:]
         
         let infiniteBounds = CGSize(width: CGFloat.infinity, height: CGFloat.infinity)
-        var bestFontSize: CGFloat = constrainingDimension
+        var bestFontSize: CGFloat = {
+            guard let maximumFontSize = maximumFontSize else {
+                return constrainingDimension
+            }
+            
+            return maximumFontSize < constrainingDimension ? maximumFontSize : constrainingDimension
+        }()
         
         for fontSize in stride(from: bestFontSize, through: 0, by: -1) {
             let newFont = UIFont(descriptor: fontDescriptor, size: fontSize)
@@ -39,9 +46,10 @@ extension UIFont {
     ///   - bounds: Bounds of the view to fit
     ///   - fontDescriptor: The font's descriptor
     ///   - additionalAttributes: Additional NSAttributedString attributes
+    ///   - maximumFontSize: An optional limit to the upper bounds of your maximum font size
     /// - Returns: UIFont representing the font that best fits in the bounds
-    static func bestFittingFont(for text: String, in bounds: CGRect, fontDescriptor: UIFontDescriptor, additionalAttributes: [NSAttributedString.Key: Any]? = nil) -> UIFont {
-        let bestSize = bestFittingFontSize(for: text, in: bounds, fontDescriptor: fontDescriptor, additionalAttributes: additionalAttributes)
+    static func bestFittingFont(for text: String, in bounds: CGRect, fontDescriptor: UIFontDescriptor, additionalAttributes: [NSAttributedString.Key: Any]? = nil, maximumFontSize: CGFloat? = nil) -> UIFont {
+        let bestSize = bestFittingFontSize(for: text, in: bounds, fontDescriptor: fontDescriptor, additionalAttributes: additionalAttributes, maximumFontSize: maximumFontSize)
         return UIFont(descriptor: fontDescriptor, size: bestSize)
     }
 }
@@ -50,10 +58,12 @@ extension UILabel {
     
     /// Will auto resize the contained text to a font size which fits the frames bounds.
     /// Uses the pre-set font to dynamically determine the proper sizing
-    func fitTextToBounds() {
+    ///
+    /// - Parameter maximumSize: An optional limit to max font size.
+    func fitTextToBounds(maximumSize: CGFloat? = nil) {
         guard let text = text, let currentFont = font else { return }
         
-        let bestFittingFont = UIFont.bestFittingFont(for: text, in: bounds, fontDescriptor: currentFont.fontDescriptor, additionalAttributes: basicStringAttributes)
+        let bestFittingFont = UIFont.bestFittingFont(for: text, in: bounds, fontDescriptor: currentFont.fontDescriptor, additionalAttributes: basicStringAttributes, maximumFontSize: maximumSize)
         font = bestFittingFont
     }
     
